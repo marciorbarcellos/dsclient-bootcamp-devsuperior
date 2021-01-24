5ccc945b5570a56d41344aaf2a23bbe946b2b904
Marcio Barcellos <marcio.barcellos@outlook.com>
2021-01-24 15:43:18 -0300

    SQL script

--- backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
+++ backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
@@ -20,6 +20,8 @@ public class Client implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(columnDefinition = "TEXT")
 	private String name;
 	private String cpf;
 	private Double income;
