83c5a265af43ced7517c111f89db1c67c34edc91
Marcio Barcellos <marcio.barcellos@outlook.com>
2021-01-23 22:57:18 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
@@ -1,26 +1,27 @@
 package com.devsuperior.dsclient.resources;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsclient.entities.Client;
+import com.devsuperior.dsclient.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
-public class ClientResource implements Serializable {
-	private static final long serialVersionUID = 1L;
+public class ClientResource {
+
+	@Autowired
+	private ClientService service;
 	
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll() {
-		List<Client> list = new ArrayList<>();
-		list.add(new Client(1L, "Maria", "063.642.554-63", 7500.00, null, 2));
-		list.add(new Client(2L, "Leo", "012.617.852-35", 8200.00, null, 1));
+		List<Client> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
